6e4cbf90354324ba1690a11dd837a9a544cbf542
ashwani <ab669522@gmail.com>
2024-08-11 07:49:22 +0530

    handles different cases of examples graciously now

--- header/const.h
+++ header/const.h
@@ -4,4 +4,15 @@
 const int SIZE = 70;
 const int SCALE = 8;
 
+const int LOOP_DELAY = 0;
+
+enum EXAMPLE_TYPE {
+	TWO_FLUID = 0,
+	ONE_FLUID_BASIC_CORNER = 1,
+	ONE_FLUID_BASIC_CENTER_ALLDIR = 2,
+    ORIGINAL_FLUID = 3,
+};
+
+const EXAMPLE_TYPE exampleType = ORIGINAL_FLUID;
+
 #endif
--- src/sim.cpp
+++ src/sim.cpp
@@ -1,6 +1,11 @@
+
+#include<iostream>
+#include <chrono>
+#include <thread>
+
 #include "../header/sim.h"
 #include "../header/ix.h"
-#include<iostream>
+#include "../header/const.h"
 
 const int Sim::numParticles = 10;
 
@@ -38,20 +43,58 @@ void Sim::Run() {
 			}
 		}
 
-		// if (sf::Mouse::isButtonPressed(sf::Mouse::Left))			
-		// 	this->container.AddDensity(currentMouse.y/SCALE, currentMouse.x/SCALE, 200);
-		
-		if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0) this->container.AddDensity((SIZE/2), (SIZE/2), 500);
+		switch (exampleType) 
+		{
+			case TWO_FLUID:
+			    if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0) {
+					this->container.AddDensity((SIZE/2), 1, 500);
+					this->container.AddDensity((SIZE/2), SIZE-2, 500);
+				}
+				break;
+			case ONE_FLUID_BASIC_CORNER:
+				if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0) this->container.AddDensity(SIZE/2, 1, 500);
+				break;
+			case ONE_FLUID_BASIC_CENTER_ALLDIR:
+				if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0) this->container.AddDensity((SIZE/2), (SIZE/2), 500);
+				break;
+			case ORIGINAL_FLUID:
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+					this->container.AddDensity(currentMouse.y/SCALE, currentMouse.x/SCALE, 200);
+				break;
+			default:
+				break;
+		};
 
 		currentMouse = sf::Mouse::getPosition(this->win);
 
 		float amountX = currentMouse.x - previousMouse.x;
 		float amountY = currentMouse.y - previousMouse.y;
-		// this->container.AddVelocity(currentMouse.y/SCALE, currentMouse.x/SCALE, amountY / 10, amountX / 10);
-		this->container.AddVelocity(SIZE/2, SIZE/2 + 1, 0, 10);
-		this->container.AddVelocity(SIZE/2 + 1, SIZE/2, 10, 0);
-		this->container.AddVelocity(SIZE/2, SIZE/2 - 1, 0, -10);
-		this->container.AddVelocity(SIZE/2 - 1, SIZE/2, -10, 0);
+
+		switch (exampleType) 
+		{
+			case TWO_FLUID:
+				if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0){
+					this->container.AddVelocity(SIZE/2, 2, 0, 10);
+					this->container.AddVelocity(SIZE/2, SIZE-3, 0, -10);
+				}
+				break;
+			case ONE_FLUID_BASIC_CORNER:
+				if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0) this->container.AddVelocity(SIZE/2, 2, 0, 10);
+				break;
+			case ONE_FLUID_BASIC_CENTER_ALLDIR:
+				if(this->container.GetDensityPointer()[IX(1,1,this->container.GetSize())] == 0){
+					this->container.AddVelocity(SIZE/2, SIZE/2 + 1, 0, 10);
+					this->container.AddVelocity(SIZE/2 + 1, SIZE/2, 10, 0);
+					this->container.AddVelocity(SIZE/2, SIZE/2 - 1, 0, -10);
+					this->container.AddVelocity(SIZE/2 - 1, SIZE/2, -10, 0);
+				}
+				break;
+			case ORIGINAL_FLUID:
+				this->container.AddVelocity(currentMouse.y/SCALE, currentMouse.x/SCALE, amountY / 10, amountX / 10);
+				break;
+			default:
+				break;
+		};
 
 		previousMouse = currentMouse;
 
@@ -60,6 +103,7 @@ void Sim::Run() {
 		this->container.FadeDensity(SIZE*SIZE);
 		
 		this->win.display();
+		std::this_thread::sleep_for(std::chrono::milliseconds(LOOP_DELAY));
 	}
 }
 
